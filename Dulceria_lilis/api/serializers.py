88ac539133d072191e51e0f4c74c86031f5069e2
yearshon019 <yearshon.yoain.orrego@gmail.com>
2025-12-10 00:06:05 -1200

    agregando lo que falta para la eva4

--- Dulceria_lilis/api/serializers.py
+++ Dulceria_lilis/api/serializers.py
@@ -1,3 +1,4 @@
+# api/serializers.py
 from rest_framework import serializers
 from productos.models import Producto
 
@@ -5,3 +6,22 @@ class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producto
         fields = '__all__'
+
+    def validate(self, data):
+        # Validación del nombre
+        if 'nombre' in data and len(data['nombre']) < 3:
+            raise serializers.ValidationError("El nombre debe tener mínimo 3 caracteres")
+
+        # Costo y precio no negativos
+        campos_numericos = ['costo_estandar', 'precio_venta', 'stock_minimo']
+        for campo in campos_numericos:
+            valor = data.get(campo, None)
+            if valor is not None and valor < 0:
+                raise serializers.ValidationError(f"El campo {campo} no puede ser negativo")
+
+        # IVA entre 0% y 30%
+        iva = data.get("impuesto_iva", None)
+        if iva is not None and (iva < 0 or iva > 30):
+            raise serializers.ValidationError("El IVA debe estar entre 0% y 30%")
+
+        return data
--- Dulceria_lilis/api/views.py
+++ Dulceria_lilis/api/views.py
@@ -1,20 +1,35 @@
-from django.http import JsonResponse
-from rest_framework.permissions import IsAuthenticated
-from rest_framework import viewsets
-from productos.models import Producto
+# api/views.py
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+from rest_framework import status, viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminOrReadOnly
+from django.http import Http404, JsonResponse
 from .serializers import ProductoSerializer
+from productos.models import Producto
+
 def info(request):
     return JsonResponse({
-
-  "proyecto": "vamos a la luna",
-
-  "version": "1.0",
-
-  "autor": "kevin ayala supremo"
-
-})
+        "proyecto": "Dulcería Lilis",
+        "version": "1.0",
+        "autor": "Kevin Ayala & Yearshon Orrego"
+    })
 
 class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+
+    def get_object(self):
+        try:
+            return super().get_object()
+        except Http404:
+            raise NotFound("Producto no encontrado en Dulcería Lilis")
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super().destroy(request, *args, **kwargs)
+        except Exception as e:
+            return Response(
+                {"status": 500, "error": str(e)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
